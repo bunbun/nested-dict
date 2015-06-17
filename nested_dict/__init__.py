1e961a18908217a3c973778f2c5e672679106d57
Leo Goodstadt <src@llew.org.uk>
2015-06-17 19:05:34 +0100

    Update from another dict or nested_dict works recursively to do the right thing (https://github.com/bunbun/nested-dict/issues/1)...

--- nested_dict/__init__.py
+++ nested_dict/__init__.py
@@ -4,5 +4,5 @@ from __future__ import division
 """
 `nested_dict` provides dictionaries with multiple levels of nested-ness:
 """
-__version__ = '1.6'
+__version__ = '1.61'
 from .implementation import nested_dict
